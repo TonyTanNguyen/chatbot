700291f531a82af77c6df5eff0e7cd237bc4dcab
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2025-01-16 14:49:39 +0700

    update code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,66 @@
 import streamlit as st
+from functions import *
+import json
 from openai import OpenAI
+with st.sidebar:
+    api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    if api_key:
+        client = OpenAI(api_key=api_key)
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+uploaded_file = st.file_uploader("Upload a Word Document", type=["docx"])
+# text_input = st.text_area('Or use JSON string as input',key="text_input")
+if uploaded_file:
+    submit = st.button('Send prompt')
+    if submit:
+        contents = convert_docx_to_text(uploaded_file)
+        if not api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+        response = askGPT_fine_turned(contents,client)
+        st.subheader("Response:", divider=True)
+        st.code(json.dumps(response),language="json")
+        st.title('Demo survey created from JSON')
+        questions = response['pages'][0]['questions']
+        with st.form("form",border = False):
+            for idx,question in enumerate(questions):
+                with st.container(border = True):
+                    st.write(question['question'])
+                    options = question['options']
+                    if question['type'] == "checkbox":
+                        num = 0
+                        for i in options:
+                            st.checkbox(i,key="check_box_" + str(idx) + "_" + str(num))
+                            num += 1
+                    elif "radio" in question['type']:
+                        st.radio('',options,key="radio_" + str(idx))
+                    elif question['type'] == "number":
+                        st.number_input('',min_value = question['min'] or 0, max_value= question['max'] or 99)
+                    elif question['type'] == "text":
+                        st.text_input('',key = "text_input_"+ str(idx))
+            st.form_submit_button()
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# if text_input:
+#     submit = st.button('Send prompt',key='submit_2')
+#     if submit:
+#         response = json.loads(text_input)
+#         questions = response['pages'][0]['questions']
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+#         with st.form("form",border = False):
+#             for idx,question in enumerate(questions):
+#                 with st.container(border = True):
+#                     st.write(question['question'])
+#                     options = question['options']
+#                     if question['type'] == "checkbox":
+#                         num = 0
+#                         for i in options:
+#                             st.checkbox(i,key="check_box_" + str(idx) + "_" + str(num))
+#                             num += 1
+#                     elif "radio" in question['type']:
+#                         st.radio('',options,key="radio_" + str(idx))
+#                     elif question['type'] == "number":
+#                         st.number_input('',min_value = question['min'] or 0, max_value= question['max'] or 99)
+#                     elif question['type'] == "text":
+#                         st.text_input('',key = "text_input_"+ str(idx))
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+#             st.form_submit_button()
